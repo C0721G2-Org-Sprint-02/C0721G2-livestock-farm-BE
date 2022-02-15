b04328b3cea140d7168c876c9d719ee7328c5eb2
hungnn88dn <hungnn88dn@gmail.com>
2022-02-15 16:23:19 +0700

    hungnn tao brand

--- C0721G2-livestock-farm-BE/src/main/java/com/project/livestockfarmbe/repository/cage/ITypeOfCageRepository.java
+++ C0721G2-livestock-farm-BE/src/main/java/com/project/livestockfarmbe/repository/cage/ITypeOfCageRepository.java
@@ -1,4 +1,9 @@
 package com.project.livestockfarmbe.repository.cage;
 
-public interface ITypeOfCageRepository {
+import com.project.livestockfarmbe.model.cage.TypeOfCage;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITypeOfCageRepository extends JpaRepository<TypeOfCage,Long> {
 }
--- C0721G2-livestock-farm-BE/src/main/java/com/project/livestockfarmbe/service/cage/impl/TypeOfCageServiceImpl.java
+++ C0721G2-livestock-farm-BE/src/main/java/com/project/livestockfarmbe/service/cage/impl/TypeOfCageServiceImpl.java
@@ -1,4 +1,19 @@
 package com.project.livestockfarmbe.service.cage.impl;
 
-public class TypeOfCageServiceImpl {
+import com.project.livestockfarmbe.model.cage.TypeOfCage;
+import com.project.livestockfarmbe.repository.cage.ITypeOfCageRepository;
+import com.project.livestockfarmbe.service.cage.ITypeOfCageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TypeOfCageServiceImpl implements ITypeOfCageService {
+    @Autowired
+    ITypeOfCageRepository typeOfCageRepository;
+    @Override
+    public List<TypeOfCage> findAllTypeOfCage() {
+        return this.typeOfCageRepository.findAll();
+    }
 }
