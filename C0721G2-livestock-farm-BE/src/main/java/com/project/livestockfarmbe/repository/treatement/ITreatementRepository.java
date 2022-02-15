5d9fc6292bdc82f95f902e4c8567350e3f1f676a
khaiphan101 <88071022+khaiphan101@users.noreply.github.com>
2022-02-15 15:54:52 +0700

    update be

--- C0721G2-livestock-farm-BE/src/main/java/com/project/livestockfarmbe/repository/treatement/ITreatementRepository.java
+++ C0721G2-livestock-farm-BE/src/main/java/com/project/livestockfarmbe/repository/treatement/ITreatementRepository.java
@@ -1,4 +1,33 @@
 package com.project.livestockfarmbe.repository.treatement;
 
-public interface ITreatementRepository {
+import com.project.livestockfarmbe.model.treatement.Treatement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ITreatementRepository extends JpaRepository< Treatement, String > {
+//    @Query(value = " select * \n" +
+//            " from treatements ", nativeQuery = true, countQuery = " select count(*) from treatements ")
+//    Page<Treatement> findAllTreatementsByFilter(@Param("doctor") String doctor,
+//                                                @Param("kindOfDisease")String kindOfDisease,
+//                                                @Param("cage")String cage,
+//                                                Pageable pageable);
+    @Query(value = " select * \n " +
+            " from treatements tr join individuals ind on tr.individual_id = ind.id \n " +
+            " where doctor like concat('%',trim(:doctor),'%')\n " +
+            " and kind_of_disease like concat('%',trim(:kindOfDisease),'%')\n " +
+            " and cage_id like concat('%',trim(:cage),'%')\n " +
+            " and tr.deleted = 0 ", nativeQuery = true, countQuery = " select count(*) from treatements tr join individuals ind on tr.individual_id = ind.id \n " +
+            " where doctor like concat('%',trim(:doctor),'%')\n " +
+            " and kind_of_disease like concat('%',trim(:kindOfDisease),'%')\n " +
+            " and cage_id like concat('%',trim(:cage),'%')\n " +
+            " and tr.deleted = 0 ")
+    Page<Treatement> findAllTreatementsByFilter(@Param("doctor")String doctor,
+            @Param("kindOfDisease")String kindOfDisease,
+            @Param("cage")String cage,
+            Pageable pageable);
 }
--- C0721G2-livestock-farm-BE/src/main/java/com/project/livestockfarmbe/service/treatement/ITreatementService.java
+++ C0721G2-livestock-farm-BE/src/main/java/com/project/livestockfarmbe/service/treatement/ITreatementService.java
@@ -1,4 +1,14 @@
 package com.project.livestockfarmbe.service.treatement;
 
+import com.project.livestockfarmbe.model.treatement.Treatement;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ITreatementService {
+
+
+    // 5.6.1 KhaiPN
+    Page<Treatement> findAllTreatementsByFilter(String doctor, String kindOfDisease, String cage, Pageable pageable);
+
+
 }
