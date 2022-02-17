06a7732375d3b3502d3a9bcf49954d9422d0c397
Namtranvn1991 <88070897+Namtranvn1991@users.noreply.github.com>
2022-02-17 17:28:27 +0700

    17/2

--- C0721G2-livestock-farm-BE/src/test/java/com/project/livestockfarmbe/EmployeeController_showListEmployee.java
+++ C0721G2-livestock-farm-BE/src/test/java/com/project/livestockfarmbe/EmployeeController_showListEmployee.java
@@ -10,7 +10,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
-class LivestockFarmBeApplicationTests {
+class EmployeeController_showListEmployee {
 
     @Autowired
     private EmployeeController employeeController;
